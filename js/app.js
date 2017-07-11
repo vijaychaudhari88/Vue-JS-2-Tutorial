1dd2845c83194528370d00f6123d5cb5031563e9
Vijay Chaudhari <vijaychaudhari117@gmail.com>
2017-07-11 12:22:57 +0530

    Data Binding

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@ new Vue({
     el: "#vue-app",
     data: {
         name: "Vijay Chaudhari",
-        job: "Developer"
+        job: "Developer",
+        website: "https://vuejs.org/v2/guide/",
+        websiteTag: '<a href="https://vuejs.org/v2/guide/">Vue.js Guide</a>'
     },
     methods: {
         greet: function (time) {
