2b5fc0d530ddc6265aa119c57620c933e5c19860
Vijay Chaudhari <vijaychaudhari117@gmail.com>
2017-07-11 13:47:57 +0530

    event Call methods with event object

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@ new Vue({
     el: "#vue-app",
     data: {
         name: "Vijay Chaudhari",
-        score: 55
+        score: 55,
+        x: 0,
+        y: 0
     },
     methods: {
         add: function (num) {
@@ -10,6 +12,10 @@ new Vue({
         },
         subtract: function (num) {
             this.score -= num;
+        },
+        updateXY: function (events) {
+            this.x = events.offsetX;
+            this.y = events.offsetY;
         }
     }
 });
